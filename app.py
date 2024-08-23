bd0440aa333a109ce14d451bd289d67e86de2bc1
Miguel <miguel.carachenski.neto@escola.pr.gov.br>
2024-08-23 11:44:48 -0300

    first commit

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
+import webbrowser as wb
 
-restaurantes=[{"nome":"Boteco do Chef","categoria":"Bar","ativo":True},
-              {"nome":"Comida Caseira","categoria":"Restaurante","ativo":False},
-              {"nome":"Kibe Kitchen","categoria":"Árabe","ativo":True}]
+sites=[ {"nome":"Google","endereco":"https://www.google.com/"},
+        {"nome":"Youtube","endereco":"https://www.youtube.com/?app=desktop&hl=pt"},
+        {"nome":"Classroom","endereco":"https://edu.google.com/intl/ALL_br/workspace-for-education/classroom/"}]
+
 
 def voltar_ao_menu_principal():
     input("\nDigite uma tecla para voltar ao menu principal: \n")
@@ -22,53 +24,61 @@ def opcao_invalida():
     print("Opção inválida\n")
     voltar_ao_menu_principal()
 
-def cadastrar_novo_restaurante():
+def listar_sites():
     os.system("clear")
-    nome_do_restaurante=input("digite o nome do novo restaurante: \n")
-    os.system("clear")
-    categoria_do_restaurante=input(f"digite a categoria do restaurante {nome_do_restaurante}\n")
-    restaurantes.append({"nome":nome_do_restaurante,"categoria":categoria_do_restaurante,"ativo":False})
-    print(f"\nO restaurante {nome_do_restaurante} foi cadastrado com sucesso!")
-    voltar_ao_menu_principal()
-
-def listar_restaurantes():
-    os.system("clear")
-    print("Restaurantes cadastrados:\n")
-    for restaurante in restaurantes:
-        nome_restaurante = restaurante["nome"]
-        categoria = restaurante["categoria"]
-        ativo = restaurante["ativo"]
-        print(f"-{nome_restaurante} -- {categoria} -- {ativo}")
+    print("sites cadastrados:\n")
+    for site in sites:
+        nome_site = site["nome"]
+        endereco = site["endereco"]
+        print(f"-{nome_site} --- {endereco}")
     voltar_ao_menu_principal() 
 
-def ativar_restaurante():
-    mostrar_subtitulo("Alterando o estado do restaurante")
-    nome_restaurante=input("digite o nome do restaurante que deseja ativar/desativar:\n")
-    restaurante_encontrado = False
+def abrir_site():
     os.system("clear")
-    for restaurante in restaurantes:
-        if nome_restaurante == restaurante["nome"]:
-            restaurante_encontrado = True       
-            restaurante["ativo"] = not restaurante["ativo"]
-            mensagem = f"O restaurante {nome_restaurante} foi ativado com sucesso!" if restaurante["ativo"] else f"O restaurante {nome_restaurante} foi desativado com sucesso"
-            print(mensagem)
-    if not restaurante_encontrado:
-        print("O restaurante não foi encontrado")
+    nome_site=input("digite o nome do site que deseja abrir:\n")
+    site_encontrado = False
+    os.system("clear")
+    for site in sites:
+        if nome_site == site["nome"]:
+            site_encontrado = True
+            wb.open_new_tab("https://www.google.com/")
+    if not site_encontrado:
+        print("O site não foi encontrado")
     voltar_ao_menu_principal()
+
+def cadastrar_site():
+    os.system("clear")
+    nome_do_site=input("digite o nome do novo site: \n")
+    os.system("clear")
+    endereco_do_site=input(f"digite o endereço do site {nome_do_site}\n")
+    sites.append({"nome":nome_do_site,"endereco":endereco_do_site})
+    print(f"\nO site {nome_do_site} foi cadastrado com sucesso!")
+    voltar_ao_menu_principal()
+
             
 
 def nome_app():
-    print('''
-███████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
-█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄─▄─██▀▄─██▄─██─▄█▄─▄▄▀██▀▄─██▄─▀█▄─▄█─▄─▄─█▄─▄▄─███▄─▄▄─█▄─▀─▄█▄─▄▄─█▄─▄▄▀█▄─▄▄─█─▄▄▄▄█─▄▄▄▄█─▄▄─█
-██─▄─▄██─▄█▀█▄▄▄▄─███─████─▀─███─██─███─▄─▄██─▀─███─█▄▀─████─████─▄█▀████─▄█▀██▀─▀███─▄▄▄██─▄─▄██─▄█▀█▄▄▄▄─█▄▄▄▄─█─██─█
-▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▀▄▄▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▀▄▄▀▄▄▄▀▀▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▀▀▄▄▄▄▄▀▄▄█▄▄▀▄▄▄▀▀▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▄▀▄▄▄▄▀
-''')
+    print("""
+
+░██████╗░███████╗██████╗░███████╗███╗░░██╗░█████╗░██╗░█████╗░██████╗░░█████╗░██████╗░  ██████╗░███████╗
+██╔════╝░██╔════╝██╔══██╗██╔════╝████╗░██║██╔══██╗██║██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔══██╗██╔════╝
+██║░░██╗░█████╗░░██████╔╝█████╗░░██╔██╗██║██║░░╚═╝██║███████║██║░░██║██║░░██║██████╔╝  ██║░░██║█████╗░░
+██║░░╚██╗██╔══╝░░██╔══██╗██╔══╝░░██║╚████║██║░░██╗██║██╔══██║██║░░██║██║░░██║██╔══██╗  ██║░░██║██╔══╝░░
+╚██████╔╝███████╗██║░░██║███████╗██║░╚███║╚█████╔╝██║██║░░██║██████╔╝╚█████╔╝██║░░██║  ██████╔╝███████╗
+░╚═════╝░╚══════╝╚═╝░░╚═╝╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚═════╝░╚══════╝
+
+░██████╗██╗████████╗███████╗░██████╗
+██╔════╝██║╚══██╔══╝██╔════╝██╔════╝
+╚█████╗░██║░░░██║░░░█████╗░░╚█████╗░
+░╚═══██╗██║░░░██║░░░██╔══╝░░░╚═══██╗
+██████╔╝██║░░░██║░░░███████╗██████╔╝
+╚═════╝░╚═╝░░░╚═╝░░░╚══════╝╚═════╝░
+""")
 
 def exibir_opcoes():
-    print("1-Cadastrar um restaurante")
-    print("2-Listar restaurantes")
-    print("3-Ativar um restaurante")
+    print("1-listar sites")
+    print("2-Abrir um site")
+    print("3-cadastrar um site")
     print("4-Sair do programa\n")
 
 def escolher_opcoes():
@@ -76,11 +86,11 @@ def escolher_opcoes():
         escolha=int(input("Escolha uma opção: "))
         print("Você selecionou a opção:",escolha,"\n")
         if escolha == 1:
-            cadastrar_novo_restaurante()
+            listar_sites()
         elif escolha == 2:
-            listar_restaurantes()
+            abrir_site()
         elif escolha == 3:
-            ativar_restaurante()
+            cadastrar_site()
         elif escolha == 4:
             print("Você escolheu sair do programa\n")
             desligar_app()
